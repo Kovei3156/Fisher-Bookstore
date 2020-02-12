0ae6a69557ccedbea17a875fed1dcc71e28280f7
kumaresh easwaran <kuma@kumareshs-MacBook-Pro.local>
2020-02-12 18:43:50 -0500

    try2

--- src/Routes.js
+++ src/Routes.js
@@ -9,7 +9,7 @@ export default function Routes(){
         <Switch>
             <Route path="/" exact component = {Home}/>
             <Route path="/books" exact component = {books}/>
-            <Route path="/authors" exact component = {Author}/>
+            <Route path="/author" exact component = {Author}/>
         </Switch>
     );
 }
