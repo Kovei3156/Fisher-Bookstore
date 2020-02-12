a7cf0d546cdc5ccb096c0dfbae15ca30276c9c6c
kumaresh easwaran <kuma@kumareshs-MacBook-Pro.local>
2020-02-12 09:32:11 -0500

    newe comit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
-<<<<<<< HEAD
-import {Navbar} from "react-bootstrap";
-=======
 import {Navbar, NavItem, Nav} from "react-bootstrap";
->>>>>>> 85100e3114585d769d22e6835abaaa20ee1cecb1
 import { Link } from "react-router-dom";
 import Routes from "./Routes";
 
@@ -15,8 +11,6 @@ export default class App extends Component{
         <Navbar.Brand>
           <Link to="/">Fisher Bookstore</Link>
         </Navbar.Brand>
-<<<<<<< HEAD
-=======
         <Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav"/>
           <Navbar.Collapse className="justify-content-end">
@@ -28,7 +22,6 @@ export default class App extends Component{
           </Navbar.Collapse>
         </Navbar.Brand>
         
->>>>>>> 85100e3114585d769d22e6835abaaa20ee1cecb1
       </Navbar>
       <Routes />
     </div>);
