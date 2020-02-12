37f941ecb1c6b1bd12687452e814ea05b992fffd
kumaresh easwaran <kuma@kumareshs-MacBook-Pro.local>
2020-02-12 18:46:09 -0500

    pls

--- src/Routes.js
+++ src/Routes.js
@@ -2,12 +2,14 @@ import React from "react";
 import {Route, Switch} from "react-router-dom";
 import Home from "./home/home";
 import books from "./books/books"
+import Author from "./author/author";
 
 export default function Routes(){
     return(
         <Switch>
             <Route path="/" exact component = {Home}/>
             <Route path="/books" exact component = {books}/>
+            <Route path="/author" exact component = {Author}/>
         </Switch>
     );
 }
