7886cfd1f54c645e5eb8c6845faa4ea398a41d2e
kumaresh easwaran <kuma@kumareshs-MacBook-Pro.local>
2020-02-12 18:19:06 -0500

    dj

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ export default class App extends Component{
               <NavItem>
                 <Nav.Link href="/books"> Books </Nav.Link>
               </NavItem>
-            </Nav>
+            </Navc>
           </Navbar.Collapse>
         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         <Navbar.Collapse className="justify-content-end">
