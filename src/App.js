4efdb559d2bcc45d70ef90107f9400965b9f5fbf
kumaresh easwaran <kuma@kumareshs-MacBook-Pro.local>
2020-02-17 18:50:12 -0500

    cdjskncs

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ export default class App extends Component{
             </NavItem>
           </Nav>
           </Navbar.Collapse>
-      </Navbarc>
+      </Navbar>
       <Routes />
     </div>);
   }
