8a5e16e16a8517339fef147e1c11f7a0523e5933
kumaresh easwaran <kuma@kumareshs-MacBook-Pro.local>
2020-02-19 18:19:26 -0500

    new updates

--- src/books/books.js
+++ src/books/books.js
@@ -1,13 +1,59 @@
-import React from "react";
+import React, {Component} from "react";
 import "./books.css";
+import { BookDisplay } from "./BookDisplay";
 
-export default function Books(){
-    return(
-        <div className="Books">
-            <div className="lander">
-                <h1>Books</h1>
-                <p>Welcome to Fisher Bookstore</p>
+export default class Books extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            books:[
+                {
+                    id: 1,
+                    title: "Domain Drive Design",
+                    author: "Eric Evans",
+                    isbn: "978-0321125217"
+                },
+                {
+                    id:2,
+                    title: "Accelerate",
+                    author: "Nicole Forsgren",
+                    isbn: "978-1942788331"
+                },
+                {
+                    id:3,
+                    title: "Accelerate",
+                    author: "Nicole Forsgren",
+                    isbn: "978-1942788331"
+                },
+                {
+                    id:4,
+                    title: "Life",
+                    author: "Nicole Forsgren",
+                    isbn: "978-1942788331"
+                },
+                {
+                    id:5,
+                    title: "Jazz",
+                    author: "Nicole Forsgren",
+                    isbn: "978-1942788331"
+                },
+                {
+                    id:6,
+                    title: "Bass",
+                    author: "Nicole Forsgren",
+                    isbn: "978-1942788331"
+                }
+
+            ]
+        };
+    }
+    render(){
+        return(
+            <div className="Books">
+                <div className="lander">
+                    <BookDisplay books={this.state.books}/>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
