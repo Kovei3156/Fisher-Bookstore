947fd4906b167a26151dc4bb70deede87c3cb98f
kumaresh easwaran <kuma@kumareshs-MacBook-Pro.local>
2020-02-20 10:04:47 -0500

    accordion

--- src/author/AuthorCard.js
+++ src/author/AuthorCard.js
@@ -1,15 +1,22 @@
 import React from "react";
-import {Card, Button} from "react-bootstrap";
+import {Card, Button, Accordion} from "react-bootstrap";
 
 export function AuthorCard(props) {
     return(
-        <Card style={{ width: "16em"}}>
-            <Card.Img variant="top" src="https://via.placeholder.com/150"/>
-            <Card.Body>
+        <Accordion defaultActiveKey="0">
+            <Card style={{ width: "16em"}}>
+                <Card.Img variant="top" src="https://via.placeholder.com/150"/>
                 <Card.Title>{props.author.name}</Card.Title>
-                <Card.Text>Author: {props.author.detail}</Card.Text>
-                <Button variant="warning">Books By Author</Button>
-            </Card.Body>
-        </Card>
+                <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                    Click me to learn more!
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="1">
+                    <Card.Body>
+                        <Card.Text>Author: {props.author.detail}</Card.Text>
+                        <Button variant="warning">Books By Author</Button>
+                    </Card.Body>
+                </Accordion.Collapse> 
+            </Card>
+        </Accordion>
     );
 }
