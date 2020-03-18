91ad1b7a8f26707b2e8c1b37a253d041eb8879eb
Kovei3156 <48021842+Kovei3156@users.noreply.github.com>
2020-03-18 10:33:34 -0400

    pullsData (#40)
    Co-authored-by: kumaresh easwaran <kuma@kumareshs-MacBook-Pro.local>

--- src/books/books.js
+++ src/books/books.js
@@ -1,59 +1,21 @@
-import React, {Component} from "react";
+import React, {useEffect, useState} from "react";
 import "./books.css";
 import { BookDisplay } from "./BookDisplay";
 
-export default class Books extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            books:[
-                {
-                    id: 1,
-                    title: "Domain Drive Design",
-                    author: "Eric Evans",
-                    isbn: "978-0321125217"
-                },
-                {
-                    id:2,
-                    title: "Accelerate",
-                    author: "Nicole Forsgren",
-                    isbn: "978-1942788331"
-                },
-                {
-                    id:3,
-                    title: "Accelerate",
-                    author: "Nicole Forsgren",
-                    isbn: "978-1942788331"
-                },
-                {
-                    id:4,
-                    title: "Life",
-                    author: "Nicole Forsgren",
-                    isbn: "978-1942788331"
-                },
-                {
-                    id:5,
-                    title: "Jazz",
-                    author: "Nicole Forsgren",
-                    isbn: "978-1942788331"
-                },
-                {
-                    id:6,
-                    title: "Bass",
-                    author: "Nicole Forsgren",
-                    isbn: "978-1942788331"
-                }
-
-            ]
-        };
-    }
-    render(){
-        return(
-            <div className="Books">
-                <div className="lander">
-                    <BookDisplay books={this.state.books}/>
-                </div>
+export default function Books(props){
+    const [data, setData] = useState([]);
+    
+    useEffect(() =>{
+        fetch("https://localhost:5001/api/books/")
+        .then(response => response.json())
+        .then(data => setData(data));
+    }, [])
+    return(
+        <div className="Books">
+            <div className="lander">
+                <BookDisplay books={data}/>
             </div>
-        );
-    }
+        </div>
+    );
+    
 }
